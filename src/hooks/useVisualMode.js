724561d0000f09738a08ba9328727383aa3d9fce
max k <maxkuhn@gmail.com>
2022-08-13 21:20:46 -0700

    useVisualMode now passes tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,30 +1,43 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+// Heavily reliant on @cwarcup's work in a number of areas.
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = function (newMode) {
-    console.log("TRANSITION========");
-    console.log("Current mode is", mode + ". newMode is", newMode);
-    console.log("History is", history);
-    console.log("Switching to", newMode);
-    let newHistory = [...history, mode];
-    console.log("Attempting to set history to", newHistory);
-    setHistory(newHistory);
-    console.log("History is now", history);
     setMode(newMode);
-    console.log("Current mode is", mode);
+    // DID NOT WORK BECAUSE I was setting it to [...history, mode] instead of [...history, newMode]
+    // why did I need to add newMode to history?
+    // did it not grab the new mode in time? <--------
+    setHistory([...history, newMode]);
   };
 
-  const back = () => {
-    console.log("CALLING BACK");
-    console.log("History is", history);
-    console.log("Going back from", mode, "to", history[history.length - 1]);
-    let lastMode = history[history.length - 1];
-    setMode(lastMode);
-    console.log("Mode is now", mode);
+  const back = function () {
+    if (history.length <= 1) {
+      setMode(initial);
+      setHistory([initial]);
+    } else {
+      let newHistory = [...history]; // copies history to new "dead" array
+      newHistory.pop(); // removes most recent entry in copied history
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
   };
 
   return { mode, transition, back };
 }
+
+// WHY DIDN'T THIS WORK?!!?
+
+// const back = function () {
+//   if (history.length <= 1) {
+//     setMode(initial);
+//   } else {
+//     let lastMode = history[history.length - 1];
+//     console.log("lastMode is", lastMode);
+//     setMode(lastMode);
+//     console.log("Mode is now", mode);
+//   }
+// };
